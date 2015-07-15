9786de9f3c058d9907cdbe755eae45cf2769a83d
Jesús Garrido Moscoso <jasus_7@hotmail.com>
2015-07-15 17:45:29 +0200

    Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz' });
